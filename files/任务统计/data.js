21d6a1312363d120a46935d9fe0b26d40a0c958b
cheer <1479437773@qq.com>
2019-03-12 17:05:20 +0800

    APP权限设置

---
+++ files/任务统计/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m,n],o,_(p,q,r,s,t,u,v,_(),w,_(x,y,z,A,B,_(C,D,E,F),G,null,H,I,J,I,K,L,M,null,N,O,P,Q,R,S,T,O),U,_(),V,_(),W,_(X,[])),Y,_(),Z,_());}; 
+var b="url",c="任务统计.html",d="generationDate",e=new Date(1552381186317.61),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="sfz",l="sfz_n",m="login",n="tips",o="page",p="packageId",q="35bdca5c39b74305afe2bf99aecc5578",r="type",s="Axure:Page",t="name",u="任务统计",v="notes",w="style",x="baseStyle",y="627587b6038d43cca051c114ac41ad32",z="pageAlignment",A="center",B="fill",C="fillType",D="solid",E="color",F=0xFFF2F2F2,G="image",H="imageHorizontalAlignment",I="near",J="imageVerticalAlignment",K="imageRepeat",L="auto",M="favicon",N="sketchFactor",O="0",P="colorStyle",Q="appliedColor",R="fontName",S="Applied Font",T="borderWidth",U="adaptiveStyles",V="interactionMap",W="diagram",X="objects",Y="masters",Z="objectPaths";
+return _creator();
+})());
